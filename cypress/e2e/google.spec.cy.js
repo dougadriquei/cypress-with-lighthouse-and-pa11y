b02ba5cc8a1eaafed0dee0d81b7b06d7d882e918
dougadriquei <dougaq@gmail.com>
2024-05-07 15:11:29 -0300

    comentarios

--- cypress/e2e/google.spec.cy.js
+++ cypress/e2e/google.spec.cy.js
@@ -1,29 +1,32 @@
 describe('Lighthouse', () => {
   it('should run performance audits in google home page', () => {
+    //Visita o site do google
     cy.visit('https://google.com.br/');
     
-        //Define os parametros considerados como aprovados para o testes.
-        const customThresholds = {
-          performance: 30,
-          accessibility: 50,
-          seo: 70,
-          'first-contentful-paint': 3000,
-          'largest-contentful-paint': 10000,
-          'cumulative-layout-shift': 0.1,
-          'total-blocking-time': 800,
-        };
-    
-        const desktopConfig = {
-          formFactor: 'desktop',
-          screenEmulation: { disabled: true },
-        };
-  
-        const lighthouseConfig = {
-          settings: { output: "html" },
-          extends: "lighthouse:default",
-        };
+    //Define os parametros considerados como aprovados para o testes.
+    const customThresholds = {
+      performance: 30,
+      accessibility: 50,
+      seo: 70,
+      'first-contentful-paint': 3000,
+      'largest-contentful-paint': 10000,
+      'cumulative-layout-shift': 0.1,
+      'total-blocking-time': 800,
+    };
+    //Define configurações para o lighthosue
+    const desktopConfig = {
+      formFactor: 'desktop',
+      screenEmulation: { disabled: true },
+    };
 
-        cy.lighthouse(customThresholds, desktopConfig, lighthouseConfig);
-        cy.pa11y();
+    //Define o formato da saida do relatório do lighthouse
+    const lighthouseConfig = {
+      settings: { output: "html" },
+      extends: "lighthouse:default",
+    };
+    //Executa o lighthouse
+    cy.lighthouse(customThresholds, desktopConfig, lighthouseConfig);
+    //Executa o pa11y
+    cy.pa11y();
   });
 });
